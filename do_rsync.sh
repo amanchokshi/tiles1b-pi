aa2d5bd73c0314cba4374bc00bd0f7ef8c1a4129
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-09-11 15:56:44 +0800

    New rsync to sort files into ozstar

--- do_rsync.sh
+++ do_rsync.sh
@@ -1,1 +1,17 @@
-rsync -avzhe ssh ./data/* achokshi@ozstar.swin.edu.au:/fred/oz048/achokshi/mwa_sats/data/tiles1b
+TODAY=`date +'%Y-%m-%d'`
+YESTERDAY=`date +'%Y-%m-%d' -d '-1 day'`
+
+
+for i in $TODAY $YESTERDAY
+do
+    for j in "S33XX" "S33YY" "S34XX" "S34YY" "S35XX" "S35YY" "S36XX" "S36YY"
+    do
+        FILES=`ls /home/jline/data/*/$j*$i*`
+        mkdir -p /home/jline/data/$j/$i
+        mv $FILES /home/jline/data/$j/$i
+    done
+    rm -r /home/jline/data/$i*
+done
+
+rsync -avzhe ssh /home/jline/data/* achokshi@ozstar.swin.edu.au:/fred/oz048/achokshi/mwa_sats/data/tiles
+#rsync -avzhe ssh ./data/* achokshi@ozstar.swin.edu.au:/fred/oz048/achokshi/mwa_sats/data/tiles1b
